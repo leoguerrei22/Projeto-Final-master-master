e8d9ae9fcb10812cac894790cedf19b224d5ad69
leoguerrei22 <123018913+leoguerrei22@users.noreply.github.com>
2023-05-16 22:42:51 +0100

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import express from 'express';
 import routes from './routes';
+import express, { NextFunction, Request, Response } from "express";
+import { verifyToken } from './middleware/authenticate';
+
 
 const app = express();
 
 // Middlewares
 app.use(express.json()); // for parsing application/json
 
+//!!/ recomendação do gepeto de usar isso num middleware a parte para evitar confusão no caso do codigo aumentar
+
+const requestLogger = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  console.log(`[${request.method}] => url:: ${request.url}`);
+
+  next();
+};
+
+app.use(requestLogger);
+
+app.use(verifyToken);
 // Routes
 app.use(routes);
 
